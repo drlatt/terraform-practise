d015bb512f7e3b5d1aeb11ec53960cf627845623
lateef <lateef.koshemani@konga.com>
2017-12-08 12:31:39 +0100

    added provisioner

--- instance.tf
+++ instance.tf
@@ -1,11 +1,16 @@
 # instance properties
 
 resource "aws_instance" "test1" {
-    ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
-    instance_type = "t2.micro"
-    subnet_id     = "${var.AWS_SUBNET_ID}"
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  subnet_id     = "${var.AWS_SUBNET_ID}"
+
+  # use the local-exec provisioner to run commands locally
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
-    instance = "${aws_instance.test1.id}"
+  instance = "${aws_instance.test1.id}"
 }
