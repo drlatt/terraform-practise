9586f1e54c79fce7448709297bf681277159861b
lateef <lateef.koshemani@konga.com>
2017-12-08 16:34:58 +0100

    added functionality to get output

--- instance.tf
+++ instance.tf
@@ -6,11 +6,16 @@ resource "aws_instance" "test1" {
   subnet_id     = "${var.aws_subnet_id}"
 
   # use the local-exec provisioner to run commands locally
-  provisioner "local-exec" {
+  /*  provisioner "local-exec" {
     command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
-  }
+  } */
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.test1.id}"
+}
+
+# output variables such as IP address
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
 }
